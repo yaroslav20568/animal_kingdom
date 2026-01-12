3bb3b98cd8a539f308ba214566cd20c042bbfbd2
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-01-12 07:37:28 -0500

    feat: init app

---
+++ android/app/src/main/kotlin/com/example/animal_kingdom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animal_kingdom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animal Kingdom',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('Welcome to Animal Kingdom')),
+      ),
+    );
+  }
+}
